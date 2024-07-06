58d0c4e63a4dd12178d9a5a45e6a494bc19acf61
yusen.yek <xhihha1@hotmail.com>
2024-07-06 11:03:38 +0800

    update doc

--- run.bat
+++ run.bat
@@ -6,7 +6,7 @@ REM 等待服务器启动
 ping 127.0.0.1 -n 3 > nul
 
 REM 在 Chrome 浏览器中打开网页
-start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8000
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8000/bible.html
 
 REM 保持命令行窗口打开
 pause
